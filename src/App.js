93de7228a6ee09bb3dedbfd261637a7245af63bb
winder001 <julioviloria.btc@gmail.com>
2021-05-03 10:30:06 -0400

    agramos la parte de terminos y condiciones

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 const App = () => {
   return (
         <main>
-            <Formulario action="">
+            <Formulario action=" nombre">
               <div>
-            
                    <Label htmlFor="">Usuario</Label>
                   <GrupoInput>
-                      <Input type="text" placeholder="usuario"/>
+                      <Input type="text" placeholder="usuario" id="nombre"/>
                       <IconoValidacion icon={faCheckCircle}/>
                   </GrupoInput>
 
                   <LeyendaError>la leyenda error del input </LeyendaError>
-              
+              </div>
+
+              <div>
+                  <Label>
+                      <input type="checkbox" name="terminos" id="terminos"/>
+                        Acepto los Terminos y Condiciones
+                  </Label>
               </div>
           </Formulario>
         </main>
